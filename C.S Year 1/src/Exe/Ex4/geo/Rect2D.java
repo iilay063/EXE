6bfd48a330a99bae91288ebc4ce0d288c2c916a1
iilay063 <36760619+iilay063@users.noreply.github.com>
2023-01-07 17:07:37 +0200

    Add files via upload

--- C.S Year 1/src/Exe/Ex4/geo/Rect2D.java
+++ C.S Year 1/src/Exe/Ex4/geo/Rect2D.java
@@ -5,69 +5,71 @@ import java.awt.geom.Rectangle2D;
 /**
  * This class represents a 2D rectangle (NOT necessarily axis parallel - this shape can be rotated!)
  * Ex4: you should implement this class!
- * @author I2CS
  *
+ * @author I2CS
  */
 public class Rect2D implements GeoShapeable {
 
-	private Point2D _firstPoint;
-	private Point2D _secondPoint;
-	public Rect2D(Point2D p1 , Point2D p2){
-		this._firstPoint = new Point2D(p1);
-		this._secondPoint = new Point2D(p2);
+    private Point2D _firstPoint;
+    private Point2D _secondPoint;
 
-	}
+    public Rect2D(Point2D p1, Point2D p2) {
+        this._firstPoint = new Point2D(p1);
+        this._secondPoint = new Point2D(p2);
 
-	@Override
-	public boolean contains(Point2D ot) {
+    }
 
-		return false;
-	}
+    @Override
+    public boolean contains(Point2D ot) {
 
-	@Override
-	public double area() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
-	@Override
-	public double perimeter() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+        return false;
+    }
 
-	@Override
-	public void move(Point2D vec) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public double area() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public GeoShapeable copy() {
-		Point2D firstPoint = new Point2D(_firstPoint.x(), _firstPoint.y());
-		Point2D secondPoint = new Point2D(_secondPoint.x(), _secondPoint.y());
-		return new Rect2D(firstPoint , secondPoint );
-	}
+    @Override
+    public double perimeter() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public void scale(Point2D center, double ratio) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void move(Point2D vec) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void rotate(Point2D center, double angleDegrees) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	@Override
-	public Point2D[] getPoints() {
-		Point2D[] pCollection = new Point2D[2];
-		pCollection[0] =new Point2D(this._firstPoint);
-		pCollection[1] = new Point2D(this._secondPoint);
+    @Override
+    public GeoShapeable copy() {
+        Point2D firstPoint = new Point2D(_firstPoint.x(), _firstPoint.y());
+        Point2D secondPoint = new Point2D(_secondPoint.x(), _secondPoint.y());
+        return new Rect2D(firstPoint, secondPoint);
+    }
 
-		return pCollection;
-	}
+    @Override
+    public void scale(Point2D center, double ratio) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void rotate(Point2D center, double angleDegrees) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Point2D[] getPoints() {
+        Point2D[] pCollection = new Point2D[2];
+        pCollection[0] = new Point2D(this._firstPoint);
+        pCollection[1] = new Point2D(this._secondPoint);
+
+        return pCollection;
+    }
 
 }
--- C.S Year 1/src/Exe/Ex4/geo/Triangle2D.java
+++ C.S Year 1/src/Exe/Ex4/geo/Triangle2D.java
@@ -3,10 +3,20 @@ package Exe.Ex4.geo;
 /**
  * This class represents a 2D Triangle in the plane.
  * Ex4: you should implement this class!
- * @author I2CS
  *
+ * @author I2CS
  */
-public class Triangle2D implements GeoShapeable{
+public class Triangle2D implements GeoShapeable {
+	private Point2D _firstPoint;
+	private Point2D _thirdPoint;
+
+	private Point2D _secondPoint;
+
+	public Triangle2D(Segment2D firstLine, Segment2D secondLine) {
+		this._firstPoint = firstLine.getPoints()[0];
+		this._secondPoint = firstLine.getPoints()[1];
+		this._thirdPoint = secondLine.getPoints()[1];
+	}
 
 	@Override
 	public boolean contains(Point2D ot) {
@@ -29,7 +39,7 @@ public class Triangle2D implements GeoShapeable{
 	@Override
 	public void move(Point2D vec) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -41,19 +51,23 @@ public class Triangle2D implements GeoShapeable{
 	@Override
 	public void scale(Point2D center, double ratio) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void rotate(Point2D center, double angleDegrees) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Point2D[] getPoints() {
-		// TODO Auto-generated method stub
-		return null;
+		Point2D[] pCollection = new Point2D[3];
+		pCollection[0] = new Point2D(this._firstPoint);
+		pCollection[1] = new Point2D(this._secondPoint);
+		pCollection[2] = new Point2D(this._thirdPoint);
+
+		return pCollection;
 	}
-	
+
 }
